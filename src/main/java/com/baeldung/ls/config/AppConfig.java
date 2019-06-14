4b3b5dc18c9b63705d745fb6a0e8ee4c60e38eb8
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2019-06-14 22:34:53 +0700

    Spring Boot Auto-Configuration

--- src/main/java/com/baeldung/ls/config/AppConfig.java
+++ src/main/java/com/baeldung/ls/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.baeldung.ls.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,5 +14,11 @@ public class AppConfig {
     @Bean(initMethod = "initialize")
     public BeanB beanB(){
         return new BeanB();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper()
+    {
+        return new ObjectMapper();
     }
 }
