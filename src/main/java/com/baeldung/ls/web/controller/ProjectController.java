8eaef9c39b4bfd12307b61492b5f15a6fa50c327
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2019-06-17 12:37:35 +0700

    What is MVC and Spring MVC

---
+++ src/main/java/com/baeldung/ls/web/controller/ProjectController.java
@@ -0,0 +1,19 @@
+package com.baeldung.ls.web.controller;
+
+import com.baeldung.ls.persistence.model.Project;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping(value = "/projects")
+public class ProjectController {
+
+    @GetMapping("/1")
+    public Project findOne()
+    {
+        return new Project("Name", LocalDate.now());
+    }
+}
